0c6b1fef5a66ae6f89b40ef0658aeb1248a84a2f
Documenter.jl <documenter@juliadocs.github.io>
2024-05-31 13:20:45 +0000

    build based on e8156c4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Llama2","category":"page"},{"location":"#Llama2","page":"Home","title":"Llama2","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Llama2.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Llama2]","category":"page"}]
+}
