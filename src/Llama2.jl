04cd41ab02a295b64203f3cfe3a380a9a5829cc2
Johanna Giese <71645106+johanni2000@users.noreply.github.com>
2024-06-08 16:37:18 +0200

    Setup structs (#17)
    * First implementation of structs (no tests)
    
    * Tests for Config and Transformer Weights
    
    * Test for RunState
    
    * Deleted unnecessary package
    
    * Fixed typos

--- src/Llama2.jl
+++ src/Llama2.jl
@@ -1,7 +1,11 @@
 module Llama2
 
+include("config.jl")
+include("transformer.jl")
 include("tokenizer.jl")
 
 export Tokenizer
+export Config
+export Transformer, RunState, TransformerWeights
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "Llama2.jl" begin
     include("test_tokenizer.jl")
+    include("test_config.jl")
+    include("test_transformer.jl")
 end
