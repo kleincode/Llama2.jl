10242dc188ca1afb305a4a8c28223d8dda9ce7d7
kleincode <f.kleinst10810@gmail.com>
2024-06-08 18:39:56 +0200

    move encode to tokenizer.jl, remove unnecessary variables, clean up code, fix small mistakes

--- src/tokenizer.jl
+++ src/tokenizer.jl
@@ -60,3 +60,72 @@ function Tokenizer(tokenizer_path::String, vocab_size::Int)
     end
     return Tokenizer(tokens, vocab_scores)
 end
+
+"""
+Encode the string text (input) into an upper-bound preallocated token array
+bos != 0 means prepend the BOS token (=1), eos != 0 means append the EOS token (=2)
+
+llama2.c correspondence: encode (l. 452)
+"""
+function encode(tokenizer::Tokenizer, text::Vector{String})
+    # temporary var that stores merge candidates of two consecuritve tokens
+    tokens::Vector{Int32} = []
+
+    # if the text is empty there is no encoding so we can return the empty tokens array
+    if isempty(text)
+        return tokens
+    end
+    # add dummy_prefix is default
+    # add prefix to the input string only if text isn't empty
+    # not sure why though
+    if text != ""
+        dummy_prefix::Int32 = tokenizer.token_to_index[" "]
+        push!(tokens, dummy_prefix)
+    end
+
+    # process the bytes of the text
+    # no need for str_buffers (as in llama2.c) because of julias native codeunits
+    for c in codeunits(text)
+        # add token of the byte to the tokens list
+        token = String(UInt8[c])
+        push!(tokens, tokenizer.token_to_index[token])
+    end
+
+    # merge the best consecutive pair each iteration
+    while true
+        best_score::Float32 = -1e10
+        best_id::Int32 = -1
+        best_idx::Int32 = -1
+
+        for i in 1:(length(tokens) - 1)
+            # merge consecutive tokens 
+            merged_token =
+                tokenizer.index_to_token[tokens[i]] *
+                tokenizer.index_to_token[tokens[i + 1]]
+
+            # check if merged_token exists
+            id::Int32 = get(tokenizer.token_to_index, merged_token, nothing)
+
+            # if id exists and the vocab_score is bigger than the best score, this token becomes the new best token
+            if !isnothing(id) && tokenizer.vocab_scores[id] > best_score
+                best_score = tokenizer.vocab_scores[id]
+                best_id = id
+                best_idx = i
+            end
+        end
+
+        # if no more tokens can be merged, we break
+        if best_idx == -1
+            break
+        end
+
+        # merge consecutive pair into new token best_id
+        tokens[best_idx] = best_id
+
+        # delete token at position best_idx+1, shift entire sequence back 1
+        splice!(tokens, best_idx + 1)
+    end
+
+    # return encoded tokens
+    return tokens
+end
